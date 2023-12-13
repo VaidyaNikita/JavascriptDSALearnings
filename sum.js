394a493f427fed5eaccc2091ddba88ea39e09f14
Nikita Vaidya <nikitavaidya.er@gmail.com>
2023-12-13 22:41:26 +0530

    sum.js is modified

--- sum.js
+++ sum.js
@@ -1,4 +1,30 @@
 let a = 10;
 let b =20;
 let x = a+b;
-console.log(c)
+console.log(x)
+
+// replacing variable values
+
+let m = 101;
+let n = 99;
+let o = m;
+m = n;
+n = o;
+console.log(m) // 99
+console.log(n) //101
+
+// concat
+
+let str1 = 'hello ';
+let str2 = 'world';
+let str = str1.concat( str2);
+console.log(str)
+
+//concatenation or coersion
+
+console.log(1+2) //------3
+console.log("1"+"2")  //-----12
+console.log("1"-"2")  //------1
+console.log("a"+"2")  //------a2
+console.log("2"+"a"+"2")  //------2a2
+console.log("a"-"2")  //------NaN --- not a number, '-' saved for numeric operation in js
