1765daf912e2b189b4f934d3ea3216f8d6eebab2
Nikita Vaidya <nikitavaidya.er@gmail.com>
2023-12-20 18:48:13 +0530

    modified jsDSA.js and sortnumber.js

--- jsDSA.js
+++ jsDSA.js
@@ -1,4 +1,4 @@
-
+// sum of passed number as argument
 function sum(n){
     let sum = 0;
     for(let i = 0; i<=n; i++){
@@ -6,6 +6,8 @@ function sum(n){
     }
     return sum;
 }
+
+
 console.log(sum(4)) // ---------1+2+3+4 = 10
 
 //sum of digits in number
@@ -13,8 +15,8 @@ console.log(sum(4)) // ---------1+2+3+4 = 10
 function sumOfDigits(num){
     let sum = 0;
     while(num > 0){
-        sum += num%10;
-        num = Math.floor(num/10)
+        sum += num%10;           //1876%10 =>6, 187%10=>7, 18%10=>8, 1%10=> 1
+        num = Math.floor(num/10) // 1876/10 =>187, 187/10=> 18, 18/10=>1, 1/10 =>0
     }
     return sum
 }
@@ -52,3 +54,23 @@ function duplicate(arr){
 }
 let arr = [1,2,3,4]
 console.log(duplicate(arr))
+
+
+//given an array of numbers and a target is su of two array element, 
+//create a funtion that will return index postion of sum og taget elements
+//ex- 4+5 = 9 => return [1,2] 
+
+let numbers = [3,4,5,6]
+
+const sumOfNums = (arr,target) =>{
+    for(let i = 0; i< arr.length; i++){
+        for(let j = i+1; j<arr.length; j++){
+            if(arr[i] + arr[j] == target){
+                return[i,j];
+            }
+        }
+    } 
+    return [];
+}
+
+console.log(sumOfNums(numbers,10))
--- sortnumber.js
+++ sortnumber.js
@@ -1,3 +1,89 @@
+//Sorting numbers using array sort() method
+
 let nums = [5,3,6,1,8,9,2];
-let sorted = nums.sort();
-console.log(sorted)
+let sorted = nums.sort((a,b)=> b-a);  //---- decending order
+// let sorted = nums.sort((a,b)=> a-b); // ---- assending order
+console.log(sorted);
+
+// find largest number among three
+
+const findLargest = (a,b,c) =>{
+    if(a >= b && a>= c){
+        console.log(`${a} is largest than ${b} & ${c}`)
+    }
+    else if(b >= a && b >= c){
+        console.log(`${b} is largest than ${a} & ${c}`)
+    }
+    else{
+        console.log(`${c} is largest than ${a} & ${b}`)
+    }
+}
+findLargest(20,22,13)
+
+//Simple calculator
+
+const calculator = (num1, operator, num2) => {
+    let result;
+    if(operator == "+"){
+        result = num1 + num2;
+    }
+    else if(operator == "-"){
+        result = num1 - num2
+    }
+    else if(operator == "*"){
+        result = num1 * num2
+    }
+    else{
+        result = num1 / num2
+    }
+    return result
+}
+
+console.log(calculator(10,'+',5))
+console.log(calculator(10,'-',5))
+console.log(calculator(10,'*',5))
+console.log(calculator(10,'/',5))
+
+//caluclator using switch case
+
+const switchCalculator = (a, op, b) =>{
+    let output;
+    switch(op){
+        case "+":
+            output = a + b;
+            console.log(`${a} + ${b} = ${output}`);
+            break;
+        case "-":
+            output = a - b;
+            console.log(`${a} - ${b} = ${output}`);
+            break;
+        case "*":
+            output = a * b;
+            console.log(`${a} * ${b} = ${output}`);
+            break;
+        case "/":
+            output = a / b;
+            console.log(`${a} / ${b} = ${output}`);
+            break;
+        default:
+            console.log("Invalid operator")    
+    }
+}
+switchCalculator(20,"+",5);
+switchCalculator(20,"-",5);
+switchCalculator(20,"*",5);
+switchCalculator(20,"/",5);
+
+//finding HCF- highest comon factor between two numbers
+//ex -- 12 is comon divisible number for 60 and 72
+
+const findHCF = (number1, number2) =>{
+    let hcf;
+    for(let i = 1; i< number1 && i < number2; i++){
+        if(number1 % i == 0 && number2 % i == 0){
+            hcf = i;
+        }
+    }
+    return hcf;  
+}
+console.log(findHCF(60,72))
